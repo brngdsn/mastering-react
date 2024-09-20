7ea1b2ddc0e6de28218bb8cd32e14b5940c928e9
brn <brn.gdsn@gmail.com>
2024-09-20 13:43:23 -0400

    added error boundary

--- task-manager-app/src/index.js
+++ task-manager-app/src/index.js
@@ -5,13 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import TaskProvider from './context/TaskProvider';
 import { BrowserRouter as Router } from 'react-router-dom';
+import ErrorBoundary from './components/ErrorBoundary';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <TaskProvider>
       <Router>
-        <App />
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
       </Router>
     </TaskProvider>
   </React.StrictMode>
